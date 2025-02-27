3362bac2331eb862ef48fdb355217171f305a0ed
Ahmed Obied <ahmedobiedahmedobied@gmail.com>
2025-02-27 20:45:39 +0200

    Initial commit

--- js/login.js
+++ js/login.js
@@ -1,17 +1,17 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const users = {
-        'AhmedObied': '32904617',
-        'RahmaSaad': '12345678',
+        'ahmedobied': '32904617',
+        'rahmasaad': '12345678',
         'quest': '00000000'
     };
-    const username = document.getElementById('username').value.trim();
-    const password = document.getElementById('password').value.trim();
+    const username = document.getElementById('username').value.trim().toLowerCase();
+    const password = document.getElementById('password').value.trim().toLowerCase();
     const message = document.getElementById('message');
 
-    // Sanitize input (remove spaces) for comparison
-    const sanitizedUsername = username.replace(/\s+/g, '');
-    const sanitizedPassword = password.replace(/\s+/g, '');
+    // Convert input to lowercase for comparison
+    const sanitizedUsername = username.replace(/\s+/g, '').toLowerCase();
+    const sanitizedPassword = password.replace(/\s+/g, '').toLowerCase();
 
     if (users[sanitizedUsername] && users[sanitizedUsername] === sanitizedPassword) {
         window.location.href = 'success.html';
